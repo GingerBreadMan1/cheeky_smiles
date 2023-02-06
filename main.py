6c74d546f9a8af93467837f05545fecb92c52f14
GingerBreadMan1 <122408023+GingerBreadMan1@users.noreply.github.com>
2023-02-06 15:32:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ def party_invoice():
     # input each addon as a string with the quantity and type
     # seperate each addon with a comma
     addons = input("What are the party addons?")
+    print(addons_list)
     
     # use the "split" method to convert the addons to a list
     addons_list = addons.split(",")
+    print(addons_list)
     
     for addon in addons_list:
         
         item = addon[-1]
         count = addon[:-1]
-        
+        print("item: ", item, "count: ", count)
     
     
     
